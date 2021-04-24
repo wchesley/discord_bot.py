3476e09619024713bd97a7db3221e27f997741dc
Walker Chesley <chesley.walker@gmail.com>
2021-04-24 12:00:34 -0500

    updated tests

--- test/test_valheim_log.py
+++ test/test_valheim_log.py
@@ -19,37 +19,43 @@ class TestValheimLog(unittest.TestCase):
         self.test_log_message_char_death = 'Got character ZDOID from Halfdan : 0:0'
         self.test_log_connection_message = 'Connections 1 ZDOS:130588  sent:0 recv:422'
         self.test_log_disconnect_message = 'Closing Socket 76561197999876368'
+        self.steamID = "76561197999876368"
         self.test_dog = ValheimLogDog.data = {
             'SteamID':'',
             'ZDOID':'',
             'steam_login_time':'',
             'ZDOID_login_time':'',
             'online':False,
-        } 
+        }
+        self.LogDogObject = ValheimLogDog('bot')
 
     def test_SteamIDFromValheimLogMessage(self):
-        steamID = ValheimLogDog.extract_log_parts(ValheimLogDog, self.test_log_message_Steam, self.test_log_date_Steam)
+        steamID = ValheimLogDog.extract_log_parts(self.LogDogObject, self.test_log_message_Steam, self.test_log_date_Steam)
         print(f'steamID: {steamID}')
         self.assertEqual(steamID, '76561197999876368')
 
     def test_zDOIDFromValheimLogMessage(self):
-        zDOID = ValheimLogDog.extract_log_parts(ValheimLogDog, self.test_log_message_zDOID, self.test_log_date_zDOID)
+        zDOID = ValheimLogDog.extract_log_parts(self.LogDogObject, self.test_log_message_zDOID, self.test_log_date_zDOID)
         print(f'ZDOID: {zDOID}')
         self.assertEqual(zDOID, 'Halfdan')
 
     def test_CharacterDeathFromValheimLogMessage(self):
-        zDOID = ValheimLogDog.extract_log_parts(ValheimLogDog, self.test_log_message_char_death, self.test_log_date_zDOID)
+        zDOID = ValheimLogDog.extract_log_parts(self.LogDogObject, self.test_log_message_char_death, self.test_log_date_zDOID)
         print(f'ZDOID: {zDOID}')
         self.assertEqual(zDOID, 'Halfdan death!')
 
     def test_ConnectionMessage(self):
-        active_connectoins = ValheimLogDog.extract_log_parts(ValheimLogDog, self.test_log_connection_message, self.test_log_date_Steam)
+        active_connectoins = ValheimLogDog.extract_log_parts(self.LogDogObject, self.test_log_connection_message, self.test_log_date_Steam)
         print(f'active: {active_connectoins}, test_msg: {self.test_log_connection_message}')
         self.assertEqual(active_connectoins, '1')
 
     def test_DisconnectMessage(self):
-        disconnection = ValheimLogDog.extract_log_parts(ValheimLogDog, self.test_log_disconnect_message, self.test_log_date_Steam)
+        disconnection = ValheimLogDog.extract_log_parts(self.LogDogObject, self.test_log_disconnect_message, self.test_log_date_Steam)
         self.assertEqual(disconnection, '76561197999876368')
+
+    async def test_steamName_from_api(self):
+        steamName = await ValheimLogDog.get_steam_persona(self.LogDogObject, self.steamID)
+        self.assertEqual(steamName, "76561197999876368")
 
 if __name__ == '__main__':
     unittest.main()
