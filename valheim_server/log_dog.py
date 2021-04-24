34c77d1d1920ad744f95aa24dc873718be2bf9da
Walker Chesley <chesley.walker@gmail.com>
2021-04-24 12:01:46 -0500

    readded return statements, broke my tests
    hard to test when the method isn't returning anything to test against!

--- valheim_server/log_dog.py
+++ valheim_server/log_dog.py
@@ -12,10 +12,10 @@ import time
 import aiofiles
 import asyncio
 
-from utils import default
+from utils import default, http
 from data.mongoDB import MongoDB_Context
 from .log_parser import LogLine
-from . import steam_api
+# from . import steam_api
 from utils.default import s_print
 
 SLEEP_SECONDS = 1
@@ -91,6 +91,7 @@ class ValheimLogDog:
     def extract_log_parts(self, message, date):
         """Get the goods from the valheim log message"""
         # Trailing space on the end is intentional, needed to remove that part of the log message
+        # Return messages are used to verify data in the tests
         steam_connect_msg = 'Got connection SteamID '
         zDOID_connect = 'Got character ZDOID from '
         current_connections = 'Connections'
@@ -98,9 +99,10 @@ class ValheimLogDog:
         s_print(f'message: {message}')
         if steam_connect_msg in message:
             self.data['SteamID'] = message.replace(steam_connect_msg, '')
+            default.s_print(f"STEAMID: {self.data['SteamID']}")
             self.data['steam_login_time'] = date
-            self.get_steam_persona(self.data['SteamID'])
-            return
+            self.data['SteamName'] = self.get_steam_persona(self.data['SteamID'])
+            return self.data['SteamID']
         elif zDOID_connect in message:
             # Death message: Got character ZDOID from Bytes : 0:0
             if message[-1] == "0":
@@ -108,13 +110,15 @@ class ValheimLogDog:
                 toon = split[4] # Should be ZDOID (in game toon name)
                 # Don't want to update database while testing...
                 new_death_count = MongoDB_Context.update_death_count(1)
-                self.bot.dispatch('on_death', new_death_count, toon) ## Emmit death event: 
-                return
+                default.s_print(f'new death count: {new_death_count}')
+                # self.bot.dispatch('on_death', new_death_count, toon) ## Emmit death event: 
+                return f'{toon} death!'
             else: 
                 full_message = message.replace(zDOID_connect,'')
                 full_message = full_message.split(' ')
                 self.data['ZDOID'] = full_message[0]
                 self.data['ZDOID_login_time'] = date
+                return self.data['ZDOID']
         elif current_connections in message: 
             s_print(f'current connections: {message}')
             connections = message.split(' ')
@@ -124,6 +128,8 @@ class ValheimLogDog:
             return message.replace(disconnect,'') # Should be steamID of player who disconnected
 
     def compare_login_time(self, steam_login_time, zdoid_login_time):
+        # TODO: Convert strings to datetime objects here: 
+        # Reasoning: It's easier to deal with strings, only need actual datetime objects for this calculation: 
         time_diff = steam_login_time - zdoid_login_time
         time_diff = abs(time_diff.total_seconds()) # steam login comes first, value SHOULD be negative.
         if time_diff < 120:
@@ -131,9 +137,17 @@ class ValheimLogDog:
         else:
             return False
 
-    def get_steam_persona(self, steamID):
-        try:
-            self.data['SteamName'] = SteamIDToVanityName(steamID)
-            s_print(f"Found Steam Name: {self.data['SteamName']}")
-        except Exception as e:
-            s_print(f'Error getting Steam Name: {e}')
+    async def get_steam_persona(self, steamID):
+        # More import errors I don't feel like debugging anymore: 
+        # Instead I"ll just utalize the http class and make requests the old fashioned way
+        # SteamWebAPI: https://developer.valvesoftware.com/wiki/Steam_Web_API
+        # try:
+        #     self.data['SteamName'] = SteamIDToVanityName(steamID)
+        #     s_print(f"Found Steam Name: {self.data['SteamName']}")
+        # except Exception as e:
+        #     s_print(f'Error getting Steam Name: {e}')
+        if steamID is None or " ":
+            steamID = self.data['SteamID']
+        steam_url = f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={self.config['steam_api_key']}&steamids={steamID}"
+        response = http.get(steam_url) 
+        return await response['response']['players'][0]['personaname']
