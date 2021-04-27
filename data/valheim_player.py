cb9af40b8b6821554ab14ddb83caae9a921992dd
wchesley <wrchesley1@buffs.wtamu.edu>
2021-04-27 13:20:18 -0500

    adjusted document for player and server

--- data/valheim_player.py
+++ data/valheim_player.py
@@ -3,15 +3,11 @@ from mongoengine import *
 class Player(Document):
     steamID = StringField(max_length=17,required=True,primary_key=True)
     steam_name = StringField(max_length=48)
-    valheim_player_id = StringField(max_length=25)
+    # valheim_player_id = StringField(max_length=25)
     valheim_name = StringField(max_length=25)
     death_count = IntField(min_value=0)
-    def __init__(self, steamID, steam_name, valheim_player_id, valheim_name, death_count):
-        self.steamID = steamID
-        self.steam_name = steam_name
-        self.valheim_player_id = valheim_player_id
-        self.valheim_name = valheim_name
-        self.death_count = death_count
+    last_login_time = DateTimeField()
+    online_state = BooleanField(default=False)
 
 class TotalDeaths(Document):
     # We'll only be keeping one record of all deaths in the server, so only need one key: 472bc69c-e35b-4f5a-b3d7-999def8c4e27
@@ -21,6 +17,7 @@ class TotalDeaths(Document):
     #     self.death_count = death_count
 
 class PlayerServerStats(Document):
+    key = UUIDField(primary_key=True, default_value="242c7b80-314b-4d38-b92b-839035f62382")
     online_count = IntField(min_value=0)
     offline_count = IntField(min_value=0)
     total = IntField(min_value=0)
